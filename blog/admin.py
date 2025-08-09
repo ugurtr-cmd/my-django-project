3a6cde4096e1038ffda1effbb0c58b008cac07ab
ugurtr-cmd <ugurkezertr@gmail.com>
2025-08-09 21:11:49 +0300

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import yazi,category,SiteContent
+from .models import yazi,category
 # Register your models here.
 admin.site.register(yazi)
 admin.site.register(category)
-admin.site.register(SiteContent)
